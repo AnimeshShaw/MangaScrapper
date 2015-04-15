9fe674b57fe5b4ca3913cbdccfae4a145d32057d
AnimeshShaw <psychocoder@outlook.com>
2015-04-15 14:36:17 +0530

    added code to get list of all manga with urls and save as csv

---
+++ resources/MangaList.py
@@ -0,0 +1,18 @@
+from lxml.html import parse
+
+__author__ = 'Psycho_Coder'
+
+
+def main():
+    with open("mangalist.csv", "w") as f:
+        tree = parse("http://www.mangapanda.com/alphabetical")
+        manga_name_list = tree.xpath("//ul[@class='series_alpha']/li/a/text()")
+        manga_url_list = tree.xpath("//ul[@class='series_alpha']/li/a/@href")
+        f.write("\"Manga Name\", URL\n")
+
+        for i in range(len(manga_name_list)):
+            f.write("\"{0}\", http://www.mangapanda.com{1}\n".format(manga_name_list[i].encode('utf8'),
+                                                                     manga_url_list[i].encode('utf8')))
+
+if __name__ == "__main__":
+    main()
